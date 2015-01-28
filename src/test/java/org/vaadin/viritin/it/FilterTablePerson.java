ab0c9800a7ecf73e05b4cbf22f0701b100088d98
Matti Tahvonen <matti@vaadin.com>
2015-01-28 21:44:42 +0200

    removed obsolete imports

--- src/test/java/org/vaadin/viritin/it/FilterTablePerson.java
+++ src/test/java/org/vaadin/viritin/it/FilterTablePerson.java
@@ -4,23 +4,14 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.data.util.filter.SimpleStringFilter;
 import com.vaadin.event.FieldEvents;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.themes.ValoTheme;
 import org.apache.commons.lang3.StringUtils;
 import org.vaadin.addonhelpers.AbstractTest;
-import org.vaadin.viritin.fields.ElementCollectionField;
-import org.vaadin.viritin.fields.EnumSelect;
 import org.vaadin.viritin.fields.FilterableTable;
 import org.vaadin.viritin.fields.MTextField;
-import org.vaadin.viritin.fields.MultiSelectTable;
-import org.vaadin.viritin.form.AbstractForm;
 import org.vaadin.viritin.layouts.MVerticalLayout;
-import org.vaadin.viritin.testdomain.Address;
-import org.vaadin.viritin.testdomain.Group;
 import org.vaadin.viritin.testdomain.Person;
 import org.vaadin.viritin.testdomain.Service;
 
-import java.util.List;
 
 @Theme("valo")
 public class FilterTablePerson extends AbstractTest {
