addb9f5243f6ba280f6c39451a3c103b2ce8ffdd
Matti Tahvonen <matti@vaadin.com>
2015-07-16 01:57:20 +0300

    Completing example with "nested property editing example"
    Switched to use Hibernate validator instead of apache BV that seems to cause issues with nested properties and Vaadin.
    
    Related to #11

--- src/test/java/org/vaadin/viritin/it/MTableWithNestedProperty.java
+++ src/test/java/org/vaadin/viritin/it/MTableWithNestedProperty.java
@@ -2,26 +2,78 @@ package org.vaadin.viritin.it;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.TextField;
 import org.vaadin.addonhelpers.AbstractTest;
+import org.vaadin.viritin.MBeanFieldGroup;
 import org.vaadin.viritin.layouts.MVerticalLayout;
 import org.vaadin.viritin.NestedPropertyTest;
 import org.vaadin.viritin.fields.MTable;
+import org.vaadin.viritin.fields.MTextField;
+import org.vaadin.viritin.fields.MValueChangeEvent;
+import org.vaadin.viritin.fields.MValueChangeListener;
+import org.vaadin.viritin.form.AbstractForm;
 
 @Theme("valo")
 public class MTableWithNestedProperty extends AbstractTest {
-    
+
+    public static class Form extends AbstractForm<NestedPropertyTest.Entity> {
+
+        private TextField property = new MTextField("property");
+        // Appears not to work, core issue!? @PropertyId("detail.property")
+        private TextField nestedPropertyField = new MTextField("detail.property");
+
+        @Override
+        protected Component createContent() {
+            return new MVerticalLayout(property, nestedPropertyField,
+                    getToolbar());
+        }
+
+        @Override
+        protected MBeanFieldGroup<NestedPropertyTest.Entity> bindEntity(
+                NestedPropertyTest.Entity entity) {
+            final MBeanFieldGroup<NestedPropertyTest.Entity> mbfg = super.bindEntity(entity);
+            mbfg.bind(nestedPropertyField, "detail.property");
+            return mbfg;
+        }
+        
+        
+    }
+
     @Override
     public Component getTestComponent() {
-        
-        return new MVerticalLayout(
-                new MTable(NestedPropertyTest.getEntities(3)).withProperties(
+        final MTable<NestedPropertyTest.Entity> table = new MTable(NestedPropertyTest.getEntities(3))
+                .withProperties(
                         "id",
                         "property",
                         "numbers[2]", // third object from collection in field "integers"
-                        "detail.property", // "property" field from object in "detail" field 
+                        "detail.property", // "property" field from object in "detail" field
                         "detailList[1].detail2.property",
                         "detailList[1].moreDetails[1].property"
-                ));
+                )
+                .withHeight("300px");
+        final Form form = new Form();
+        form.setSavedHandler(
+                new AbstractForm.SavedHandler<NestedPropertyTest.Entity>() {
+
+            @Override
+            public void onSave(NestedPropertyTest.Entity entity) {
+                table.refreshRowCache();
+            }
+        });
+        
+        table.addMValueChangeListener(new MValueChangeListener<NestedPropertyTest.Entity>() {
+
+            @Override
+            public void valueChange(
+                    MValueChangeEvent<NestedPropertyTest.Entity> event) {
+                if(event.getValue() != null) {
+                    form.setEntity(event.getValue());
+                }
+            }
+        });
+
+        return new MVerticalLayout(
+                table, form);
     }
-    
+
 }
