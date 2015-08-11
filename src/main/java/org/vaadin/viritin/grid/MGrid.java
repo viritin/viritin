e6785298889262fe1d3439c09c9fec83c8b557a6
Matti Tahvonen <matti@vaadin.com>
2015-08-11 17:13:03 +0300

    Added better typed setRows methods to MGrid

--- src/main/java/org/vaadin/viritin/grid/MGrid.java
+++ src/main/java/org/vaadin/viritin/grid/MGrid.java
@@ -3,8 +3,15 @@ package org.vaadin.viritin.grid;
 import org.vaadin.viritin.grid.utils.GridUtils;
 
 import com.vaadin.ui.Grid;
+import java.util.Arrays;
+import java.util.List;
+import org.vaadin.viritin.ListContainer;
 
-public class MGrid extends Grid {
+/**
+ *
+ * @param <T> the entity type listed in the Grid
+ */
+public class MGrid<T> extends Grid {
 
     /**
      * Enables saving/loading grid settings (visible columns, sort order, etc)
@@ -15,6 +22,16 @@ public class MGrid extends Grid {
      */
     public void attachSaveSettings(String settingsName) {
         GridUtils.attachToGrid(this, settingsName);
+    }
+    
+    public MGrid<T> setRows(List<T> rows) {
+        setContainerDataSource(new ListContainer(rows));
+        return this;
+    }
+
+    public MGrid<T> setRows(T... rows) {
+        setContainerDataSource(new ListContainer(Arrays.asList(rows)));
+        return this;
     }
     
 }
--- src/test/java/org/vaadin/viritin/it/GridSaveSettingsTest.java
+++ src/test/java/org/vaadin/viritin/it/GridSaveSettingsTest.java
@@ -4,52 +4,47 @@ import org.vaadin.addonhelpers.AbstractTest;
 import org.vaadin.viritin.grid.MGrid;
 import org.vaadin.viritin.testdomain.Person;
 
-import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Grid.Column;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
+import org.vaadin.viritin.layouts.MVerticalLayout;
 
 public class GridSaveSettingsTest extends AbstractTest {
 
-	@Override
-	public Component getTestComponent() {
-		final VerticalLayout layout = new VerticalLayout();
-		MGrid table=new MGrid();
-		BeanItemContainer<Person> ds = new BeanItemContainer<Person>(
-				Person.class);
-		layout.setMargin(true);
-		layout.setSpacing(true);
-		Label info =new Label();
-		info.setValue("Hide and/or sort columns."
-				+ " Grid will save your settings in the browser cookies."
-				+ " And load them next time you open the view");
-		ds.addBean(new Person(1, "Some", "One",13));
-		ds.addBean(new Person(2, "Arni", "Red",83));
-		ds.addBean(new Person(3, "Sami", "Green",42));
-		ds.addBean(new Person(4, "Togy", "Tilly",23));
-		ds.addBean(new Person(5, "Billy", "Willy",22));
-		ds.addBean(new Person(6, "Anna", "Bunny",23));
-		ds.addBean(new Person(7, "Joe", "Black",52));
-		table = new MGrid();
-		table.setContainerDataSource(ds);
-		makeGridColumnsHideable(table);
-		makeGridOrderable(table);
-		table.setColumnReorderingAllowed(true);
-		table.attachSaveSettings("personsGrid");
-		layout.addComponent(info);
-		layout.addComponent(table);
-		
-		return layout;
-	}
-	private void makeGridOrderable(MGrid table) {
-		table.setColumnReorderingAllowed(true);
-		
-	}
-	private void makeGridColumnsHideable(Grid grid) {
-        for(Column c : grid.getColumns()) {
+    @Override
+    public Component getTestComponent() {
+        Label info = new Label();
+        info.setValue("Hide and/or sort columns."
+                + " Grid will save your settings in the browser cookies."
+                + " And load them next time you open the view");
+
+        MGrid<Person> table = new MGrid<Person>();
+        table.setRows(
+                new Person(1, "Some", "One", 13),
+                new Person(2, "Arni", "Red", 83),
+                new Person(3, "Sami", "Green", 42),
+                new Person(4, "Togy", "Tilly", 23),
+                new Person(5, "Billy", "Willy", 22),
+                new Person(6, "Anna", "Bunny", 23),
+                new Person(7, "Joe", "Black", 52)
+        );
+        makeGridColumnsHideable(table);
+        makeGridOrderable(table);
+        table.setColumnReorderingAllowed(true);
+        table.attachSaveSettings("personsGrid");
+
+        return new MVerticalLayout(info, table);
+    }
+
+    private void makeGridOrderable(MGrid table) {
+        table.setColumnReorderingAllowed(true);
+
+    }
+
+    private void makeGridColumnsHideable(Grid grid) {
+        for (Column c : grid.getColumns()) {
             c.setHidable(true);
         }
-	}
+    }
 }
