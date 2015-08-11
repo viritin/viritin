eb028be0c0a14515f7c9007fa707acd6c513e0a8
Matti Tahvonen <matti@vaadin.com>
2015-08-11 16:15:28 +0300

    reformatted MGrid

--- src/main/java/org/vaadin/viritin/grid/MGrid.java
+++ src/main/java/org/vaadin/viritin/grid/MGrid.java
@@ -4,13 +4,17 @@ import org.vaadin.viritin.grid.utils.GridUtils;
 
 import com.vaadin.ui.Grid;
 
-public class MGrid extends Grid{
+public class MGrid extends Grid {
 
-	/**
-	 * Enables saving/loading grid settings (visible columns, sort order, etc) to cookies.
-	 * @param settingsName cookie name where settings are saved should be unique.
-	 */
-	public void attachSaveSettings(String settingsName) {
-			GridUtils.attachToGrid(this, settingsName);
-	}
+    /**
+     * Enables saving/loading grid settings (visible columns, sort order, etc)
+     * to cookies.
+     *
+     * @param settingsName cookie name where settings are saved should be
+     * unique.
+     */
+    public void attachSaveSettings(String settingsName) {
+        GridUtils.attachToGrid(this, settingsName);
+    }
+    
 }
