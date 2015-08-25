33e29cfa0f60fee1194dd27c3eee955c93f971e0
Matti Tahvonen <matti@vaadin.com>
2015-08-25 13:32:46 +0300

    Enhanced example

--- src/test/java/org/vaadin/viritin/it/EnumFieldAsRadioButtonGroup.java
+++ src/test/java/org/vaadin/viritin/it/EnumFieldAsRadioButtonGroup.java
@@ -4,6 +4,7 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.data.util.ObjectProperty;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.OptionGroup;
+import com.vaadin.ui.themes.ValoTheme;
 
 import org.vaadin.addonhelpers.AbstractTest;
 import org.vaadin.viritin.fields.EnumSelect;
@@ -15,16 +16,24 @@ import org.vaadin.viritin.testdomain.Address.AddressType;
  */
 @Theme("valo")
 public class EnumFieldAsRadioButtonGroup extends AbstractTest {
-    
+
     @Override
     public Component getTestComponent() {
-        
-        EnumSelect select = new EnumSelect().withSelectType(OptionGroup.class);
-        // This typically comes form bean binding
+
+        EnumSelect<AddressType> select = new EnumSelect<AddressType>()
+                .withSelectType(OptionGroup.class);
+        select.setStyleName(ValoTheme.OPTIONGROUP_HORIZONTAL);
+
+        // The Enum type is detected when the edited property is bound to select
+        // This typically comes form basic bean binding, but here done manually.
         ObjectProperty objectProperty = new ObjectProperty(AddressType.Home);
         select.setPropertyDataSource(objectProperty);
         
+        // Alternatively, if not using databinding at all, you could just use 
+        // basic TypedSelect, or the method from it
+        // select.setOptions(AddressType.values());
+
         return select;
     }
-    
+
 }
