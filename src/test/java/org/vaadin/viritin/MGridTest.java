7ddc75726cb6f5bbb80e58776e4e9b45529829d9
Matti Tahvonen <matti@vaadin.com>
2015-09-18 16:27:51 +0300

    Cleaned test code

--- src/test/java/org/vaadin/viritin/MGridTest.java
+++ src/test/java/org/vaadin/viritin/MGridTest.java
@@ -18,14 +18,12 @@ public class MGridTest {
     public void testTypedSelection() {
         
         List<Person> listOfPersons = Service.getListOfPersons(100);
-        
-        MGrid<Person> g = new MGrid<Person>();
-        g.setRows(listOfPersons);
-        
         final Person selectedDude = listOfPersons.get(2);
 
+        MGrid<Person> g = new MGrid<Person>();
         // Awesome, now the API actually tells you what you should pass as 
         // parameters and what you'll get as return type.
+        g.setRows(listOfPersons);
         g.selectRow(selectedDude);
         Person selectedRow = g.getSelectedRow();
         
