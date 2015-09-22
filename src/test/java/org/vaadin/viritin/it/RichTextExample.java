3187494fa6e8800bd96c0a2fddfc8f927f6e2279
Matti Tahvonen <matti@vaadin.com>
2015-09-22 14:11:32 +0300

    Improving example to contain usage with LabelField

--- src/test/java/org/vaadin/viritin/it/RichTextExample.java
+++ src/test/java/org/vaadin/viritin/it/RichTextExample.java
@@ -1,11 +1,14 @@
 package org.vaadin.viritin.it;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.data.util.ObjectProperty;
 import com.vaadin.ui.Component;
 import org.jsoup.safety.Whitelist;
 import org.vaadin.addonhelpers.AbstractTest;
+import org.vaadin.viritin.fields.LabelField;
 import org.vaadin.viritin.label.RichText;
 import org.vaadin.viritin.layouts.MVerticalLayout;
+import org.vaadin.viritin.testdomain.Person;
 
 /**
  *
@@ -16,9 +19,10 @@ public class RichTextExample extends AbstractTest {
 
     @Override
     public Component getTestComponent() {
-        
+
         // note that styles is stripped of by default
-        RichText example1 = new RichText("<h1 style='color:red' class='foobar'>Jou!</h1>");
+        RichText example1 = new RichText(
+                "<h1 style='color:red' class='foobar'>Jou!</h1>");
 
         // This one will tolerate quite a lot more
         RichText example2 = new RichText("<h1 style='color:red'>Jou!</h1>") {
@@ -28,12 +32,24 @@ public class RichTextExample extends AbstractTest {
                         "style");
             }
         };
-        
-        
+
         // This one will tolerate quite a lot more
-        RichText example3 = new RichText().withMarkDown("This is **Markdown** formatted *text*");
-        
-        return new MVerticalLayout(example1, example2, example3);
+        RichText example3 = new RichText().withMarkDown(
+                "This is **Markdown** formatted *text*");
+
+        // RichText can also be used through LabelField 
+        // (and it is with default settings)
+        LabelField lf = new LabelField(String.class);
+        lf.setLabel(new RichText() {
+            @Override
+            public Whitelist getWhitelist() {
+                return Whitelist.relaxed().addAttributes("h1",
+                        "style");
+            }
+        });
+        lf.setPropertyDataSource(new ObjectProperty("<h1 style='color:blue'>Hi hou!</h1>"));
+
+        return new MVerticalLayout(example1, example2, example3, lf);
     }
 
 }
