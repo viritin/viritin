ee47cee6520f07470bc408593cdc8ce35b92ad83
Matti Tahvonen <matti@vaadin.com>
2016-01-07 13:27:18 +0200

    Fixed invalid hashcode/equals method.

--- src/test/java/org/vaadin/viritin/testdomain/Person.java
+++ src/test/java/org/vaadin/viritin/testdomain/Person.java
@@ -115,19 +115,7 @@ public class Person {
         if (getClass() != obj.getClass())
             return false;
         Person other = (Person) obj;
-        if (ObjectUtils.notEqual(age, other.age))
-            return false;
-        if (firstName == null) {
-            if (other.firstName != null)
-                return false;
-        } else if (!firstName.equals(other.firstName))
-            return false;
         if (id != other.id)
-            return false;
-        if (lastName == null) {
-            if (other.lastName != null)
-                return false;
-        } else if (!lastName.equals(other.lastName))
             return false;
         return true;
     }
@@ -136,12 +124,7 @@ public class Person {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + age;
-        result = prime * result
-                + ((firstName == null) ? 0 : firstName.hashCode());
         result = prime * result + id;
-        result = prime * result
-                + ((lastName == null) ? 0 : lastName.hashCode());
         return result;
     }
 
