bd9118c0afcc181f54615b3bf026ac2e80f046ad
Matti Tahvonen <matti@vaadin.com>
2016-02-03 17:39:06 +0200

    no message

--- src/main/java/org/vaadin/viritin/fields/MDateField.java
+++ src/main/java/org/vaadin/viritin/fields/MDateField.java
@@ -5,6 +5,7 @@ import com.vaadin.server.Resource;
 import com.vaadin.shared.ui.datefield.Resolution;
 import com.vaadin.ui.DateField;
 import java.util.Date;
+import java.util.Map;
 
 /**
  *
@@ -31,6 +32,64 @@ public class MDateField extends DateField {
         super(caption, value);
     }
 
+    public enum InitialTimeMode {
+        START_OF_DAY, END_OF_DAY, NOW
+    }
+
+    private InitialTimeMode initialTimeMode;
+
+    /**
+     * Flag to tell if setting the initial (from null to something) value by the
+     * end user
+     */
+    private boolean settingInitialValue;
+
+    /**
+     * @return the method how value of the time part is to be initialized when 
+     * resolution setting don't show time.
+     */
+    public InitialTimeMode getInitialTimeMode() {
+        return initialTimeMode;
+    }
+
+    /**
+     * 
+     * @param initialTimeMode the method how value of the time part is to be initialized when 
+     * resolution setting don't show time.
+     */
+    public void setInitialTimeMode(InitialTimeMode initialTimeMode) {
+        this.initialTimeMode = initialTimeMode;
+    }
+
+    @Override
+    public void changeVariables(Object source, Map<String, Object> variables) {
+        if (getValue() == null) {
+            settingInitialValue = true;
+        }
+        super.changeVariables(source, variables);
+        settingInitialValue = false;
+    }
+
+    @Override
+    protected void setValue(Date newValue, boolean repaintIsNotNeeded) throws ReadOnlyException {
+        if (settingInitialValue && getResolution().ordinal() > Resolution.HOUR.ordinal()) {
+            if (getInitialTimeMode() == InitialTimeMode.START_OF_DAY) {
+                newValue.setHours(0);
+                newValue.setMinutes(0);
+                newValue.setSeconds(0);
+                newValue.setTime(
+                        newValue.getTime() - newValue.getTime() % 1000l);
+            } else if (getInitialTimeMode() == InitialTimeMode.END_OF_DAY) {
+                newValue.setHours(23);
+                newValue.setMinutes(59);
+                newValue.setSeconds(59);
+                newValue.setTime(
+                        newValue.getTime() - newValue.getTime() % 1000l + 999l);
+            }
+        }
+        super.setValue(newValue, repaintIsNotNeeded);
+    }
+
     public MDateField withIcon(Resource icon) {
         setIcon(icon);
         return this;
@@ -45,12 +104,12 @@ public class MDateField extends DateField {
         setStyleName(styleName);
         return this;
     }
-    
+
     public MDateField withRequired(boolean required) {
         setRequired(required);
         return this;
     }
-    
+
     public MDateField withRequiredError(String requiredError) {
         setRequiredError(requiredError);
         return this;
