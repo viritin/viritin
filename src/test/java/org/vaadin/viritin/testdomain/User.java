573369d6d8c5a96f69f132df25b6c1f81f52597c
Klaus Sausen <RookieGuy@users.noreply.github.com>
2016-03-26 22:52:46 +0100

    added Apache 2 License headers

--- src/test/java/org/vaadin/viritin/testdomain/User.java
+++ src/test/java/org/vaadin/viritin/testdomain/User.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Klaus Sausen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.vaadin.viritin.testdomain;
 
 import java.util.Locale;
@@ -7,7 +22,6 @@ import org.vaadin.viritin.testdomain.aspect.Salutation;
 
 /** 
  * An example session user bean based on a person entity
- * @author Klaus Sausen
  */
 public class User 
 	implements LocaleAware, Salutation 
