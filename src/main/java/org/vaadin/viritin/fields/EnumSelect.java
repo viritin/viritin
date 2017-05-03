f43f622050a488971d5f7132ac530a270b9676f3
Mikhail V Buzuverov <star.guardian.mgn@gmail.com>
2017-05-03 17:09:01 +0500

    Added EnumSelect (#310)

---
+++ src/main/java/org/vaadin/viritin/fields/EnumSelect.java
@@ -0,0 +1,31 @@
+package org.vaadin.viritin.fields;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.vaadin.viritin.fluency.ui.FluentAbstractField;
+
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.ItemCaptionGenerator;
+
+public class EnumSelect<T extends Enum<T>> extends ComboBox<T> implements FluentAbstractField<EnumSelect<T>, T> {
+	private static final long serialVersionUID = 6512803610855055709L;
+
+	public EnumSelect(Class<T> enumClass) {
+		this("", enumClass);
+	}
+
+	public EnumSelect(String caption, Class<T> enumClass) {
+		super(caption, getConstantList(enumClass));		
+	}
+	
+	public EnumSelect<T> withItemCaptionGenerator(ItemCaptionGenerator<T> itemCaptionGenerator) {
+		setItemCaptionGenerator(itemCaptionGenerator);
+		return this;
+	}
+	
+	private static <T extends Enum<T>> Collection<T> getConstantList(Class<T> enumClass) {
+		final T[] constants=enumClass.getEnumConstants();
+		return Arrays.asList(constants);
+	}
+}
