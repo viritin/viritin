b61d7e82ad74eee0499d47e4e87caacb22c4e5c2
Daniel Nordhoff-Vergien <dve@vergien.net>
2020-05-05 13:03:22 +0200

    Adding a markdown field component

---
+++ viritin/src/main/java/org/vaadin/viritin/fluency/ui/FluentTextArea.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 Daniel Nordhoff-Vergien.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.vaadin.viritin.fluency.ui;
+
+import com.vaadin.data.HasValue;
+import com.vaadin.shared.ui.ValueChangeMode;
+import com.vaadin.ui.TextArea;
+
+public interface FluentTextArea<S extends FluentTextArea<S>>
+    extends FluentAbstractField<S, String>, FluentHasValueChangeMode<S> {
+  /**
+   * Sets the number of rows in the text area.
+   *
+   * @param rows the number of rows for this text area.
+   */
+  public default S withRows(int rows) {
+    ((TextArea) this).setRows(rows);
+    return (S) this;
+  }
+
+  /**
+   * Sets the text area's word-wrap mode on or off.
+   *
+   * @param wordWrap <code>true</code> to use word-wrap mode <code>false</code> otherwise.
+   */
+  public default S withWordWrap(boolean wordWrap) {
+    ((TextArea) this).setWordWrap(wordWrap);
+    return (S) this;
+  }
+
+  public default S addTextChangeListener(HasValue.ValueChangeListener<String> l) {
+    this.addValueChangeListener(l);
+    this.setValueChangeMode(ValueChangeMode.LAZY);
+    return (S) this;
+  }
+}
---
+++ viritin/src/test/java/org/vaadin/viritin/it/MarkDownFieldExample.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Daniel Nordhoff-Vergien.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.vaadin.viritin.it;
+
+import org.vaadin.addonhelpers.AbstractTest;
+import org.vaadin.viritin.button.MButton;
+import org.vaadin.viritin.fields.MarkDownField;
+import org.vaadin.viritin.layouts.MVerticalLayout;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
+
+public class MarkDownFieldExample extends AbstractTest {
+
+  @Override
+  public Component getTestComponent() {
+    MarkDownField markDownField1 =
+        new MarkDownField("MarkDownField 1").withValue("This is **Markdown** formatted *text*");
+    MarkDownField markDownField2 = new MarkDownField("With TextChangeListener").withValue(
+        "This has a textChangeListener attached")
+        .addTextChangeListener(e -> Notification.show(e.getValue(), Type.HUMANIZED_MESSAGE));
+
+    return new MVerticalLayout(markDownField1, //
+        new MButton("Toggle ReadOnly",
+            e -> markDownField1.setReadOnly(!markDownField1.isReadOnly())), //
+        markDownField2);
+  }
+
+}
